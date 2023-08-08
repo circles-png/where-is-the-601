9da36a2a8be0683a7d5a498fd9bd43b2087c817d
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-08-08 18:38:26 +1000

    add code to request the api

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use anyhow::{Ok, Result};
+use dotenvy_macro::dotenv;
+use reqwest::{
+    header::{HeaderMap, ACCEPT, AUTHORIZATION},
+    ClientBuilder, Method, Request,
+};
+use std::fs::write;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    const ENDPOINT: &str = "https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/buses";
+    let client = ClientBuilder::new()
+        .default_headers({
+            let mut header_map = HeaderMap::new();
+            header_map.append(ACCEPT, "application/x-google-protobuf".parse()?);
+            header_map.append(
+                AUTHORIZATION,
+                format!("apikey {}", dotenv!("API_KEY")).parse()?,
+            );
+            header_map
+        })
+        .build()?;
+    let request = Request::new(Method::GET, ENDPOINT.parse()?);
+    let response = client.execute(request).await?;
+    write("out/data.proto", response.bytes().await?)?;
+    Ok(())
+}
